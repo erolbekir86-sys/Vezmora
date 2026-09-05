86a88a8dc6fb507c60967c746854367e8bffd9fe
erolbekir86-sys <erolbekir86@gmail.com>
2026-09-05 12:37:01 +0200

    Register private-beta readiness diagnostics

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from . import google_ads_diagnostics as _google_ads_diagnostics
 from . import connector_empty_states as _connector_empty_states
 from . import connector_privacy_controls as _connector_privacy_controls
+from . import beta_readiness as _beta_readiness
 
 __all__ = []
