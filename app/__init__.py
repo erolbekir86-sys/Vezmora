e70d91273764bac8bd9ac048f4997e2d813e0be6
erolbekir86-sys <erolbekir86@gmail.com>
2026-09-05 12:23:47 +0200

    Register connector privacy controls

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from . import google_ads_diagnostics as _google_ads_diagnostics
 from . import connector_empty_states as _connector_empty_states
+from . import connector_privacy_controls as _connector_privacy_controls
 
 __all__ = []
