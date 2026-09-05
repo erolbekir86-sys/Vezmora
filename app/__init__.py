8f0c8e0205abff160849e6779dd5921017772045
erolbekir86-sys <erolbekir86@gmail.com>
2026-09-05 12:54:30 +0200

    Install secret-safe HTTP error handling

--- app/__init__.py
+++ app/__init__.py
@@ -2,5 +2,9 @@ from . import google_ads_diagnostics as _google_ads_diagnostics
 from . import connector_empty_states as _connector_empty_states
 from . import connector_privacy_controls as _connector_privacy_controls
 from . import beta_readiness as _beta_readiness
+from .http_error_safety import install_http_error_safety as _install_http_error_safety
+from .main import app as _app
+
+_install_http_error_safety(_app)
 
 __all__ = []
