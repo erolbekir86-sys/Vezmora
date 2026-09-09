a56ed1f08e34270fbe2e27e2de7340e743206ce6
erolbekir86-sys <erolbekir86@gmail.com>
2026-09-09 12:40:45 +0200

    Install authenticated mutation CSRF guard

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,14 @@ from . import connector_empty_states as _connector_empty_states
 from . import connector_privacy_controls as _connector_privacy_controls
 from . import account_privacy_controls as _account_privacy_controls
 from . import beta_readiness as _beta_readiness
+from .csrf_guard import install_csrf_guard as _install_csrf_guard
 from .http_error_safety import install_http_error_safety as _install_http_error_safety
 from .main import app as _app
 from .public_routing import install_public_routing as _install_public_routing
 from .runtime_diagnostics import install_runtime_diagnostics as _install_runtime_diagnostics
 
 _install_http_error_safety(_app)
+_install_csrf_guard(_app)
 _install_runtime_diagnostics(_app)
 _install_public_routing(_app)
 
